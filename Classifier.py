eff9fb516ee75bc8edbd4d922725be33f1ef120c
chloepomeroy <68558959+chloepomeroy@users.noreply.github.com>
2021-04-15 12:34:52 -0400

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from Sentiment_Analysis_Model import classifier
+from Sentiment_Analysis_Model import *
 
 from nltk.tokenize import word_tokenize
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 import emoji
 import pandas as pd
 
-df = pd.read_excel('twitter_replies_only.xls')
+data = load_workbook('twitter_replies.xlsx')
+columns = next(data['Sheet'].values)[0:]
+df = pd.DataFrame(data['Sheet'].values, columns=columns)
 
-column_names = ['brand', 'brand_post_url', 'user', 'user name', 'reply', 'date', 'time', 'sentiment']
+column_names = ['date', 'time', 'brand', 'brand_post_url', 'user', 'user name', 'reply', 'sentiment']
 wb=Workbook()
 page=wb.active
 page.append(column_names) #append the first line in the file with the column names
 
 #For each tweet in our scraped tweets, run the sentiment classifier
 for index, row in df.iterrows():
-    user_reply = row['Text']
+    user_reply = row['text']
     brand = row['brand']
     brand_post_url = row['brand_post_url']
     user = row['user']
-    reply_tokens = remove_noise(word_tokenize(emoji.demojize((user_reply)))) #remove noise, tokenize tweets, and translate emojis to words
-    sentiment = [brand, brand_post_url, user, user_reply, classifier.classify(dict([token, True] for token in reply_tokens))]
+    user_name = row['user name']
+    date = row['date']
+    time = row['time']
+    reply_tokens = remove_noise(word_tokenize(emoji.demojize((user_reply)))) #remove noise and tokenize tweets
+    sentiment = [date, time, brand, brand_post_url, user, user_name, user_reply, classifier.classify(dict([token, True] for token in reply_tokens))]
     page.append(sentiment) #append a line in the file with the tweet and sentiment result    
 
-wb.save(filename = 'twitter_sentiments1.xlsx')
+wb.save(filename = 'twitter_sentiments.xlsx')
