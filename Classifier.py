15c8be09db8361ee37dc0bf82706f7845b931f05
chloepomeroy <68558959+chloepomeroy@users.noreply.github.com>
2021-04-16 15:54:59 -0400

    Update Classifier.py

--- Classifier.py
+++ Classifier.py
@@ -13,7 +13,7 @@ columns = next(data['Sheet'].values)[0:]
 df = pd.DataFrame(data['Sheet'].values, columns=columns)
 df = df.drop([df.index[0]]) #drop the original header row
 
-column_names = ['date', 'time', 'brand', 'brand_post_url', 'user', 'user name', 'reply', 'sentiment']
+column_names = ['date', 'time', 'brand', 'brand_post_url', 'username', 'name', 'reply', 'sentiment']
 wb=Workbook()
 page=wb.active
 page.append(column_names) #append the first line in the file with the column names
@@ -23,12 +23,12 @@ for index, row in df.iterrows():
     user_reply = row['text']
     brand = row['brand']
     brand_post_url = row['brand_post_url']
-    user = row['user']
-    user_name = row['user name']
+    user = row['username']
+    user_name = row['name']
     date = row['date']
     time = row['time']
     reply_tokens = remove_noise(word_tokenize(emoji.demojize((user_reply)))) #remove noise and tokenize tweets
     sentiment = [date, time, brand, brand_post_url, user, user_name, user_reply, classifier.classify(dict([token, True] for token in reply_tokens))]
-    page.append(sentiment) #append a line in the file with the tweet and sentiment result    
+    page.append(sentiment) #append a line in the file with the tweet and sentiment result     
 
 wb.save(filename = 'twitter_sentiments.xlsx')
